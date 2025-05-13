37fc01488e0e33360db0b7e6fe6eb287e708148e
a93236 <62142130+a93236@users.noreply.github.com>
2025-05-13 14:40:37 +0100

    Update dclient.c

--- src/dclient.c
+++ src/dclient.c
@@ -8,45 +8,75 @@
 #include <sys/stat.h>
 #include "common.h"
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Uso: %s <mensagem>\n", argv[0]);
-        return 1;
-    }
-
-    pid_t pid = getpid();
-
+void send_request(Request *req, Response *res) {
+    // Criar pipe nomeado para resposta
     char client_pipe[128];
-    snprintf(client_pipe, sizeof(client_pipe), "%s%d", PIPE_SERVER_TO_CLIENT_PREFIX, pid);
+    snprintf(client_pipe, sizeof(client_pipe), "%s%d", PIPE_SERVER_TO_CLIENT_PREFIX, getpid());
+    unlink(client_pipe);
     mkfifo(client_pipe, 0666);
 
-    Request req;
-    req.client_pid = pid;
-    req.op = OP_PING;
-    snprintf(req.data, MAX_MESSAGE, "%s", argv[1]);
-
-    int fd = open(PIPE_CLIENT_TO_SERVER, O_WRONLY);
-    if (fd < 0) {
-        perror("open client_to_server");
-        return 1;
+    // Enviar pedido ao servidor
+    int server_fd = open(PIPE_CLIENT_TO_SERVER, O_WRONLY);
+    if (server_fd < 0) {
+        perror("Erro ao abrir pipe do servidor");
+        exit(1);
     }
+    write(server_fd, req, sizeof(Request));
+    close(server_fd);
 
-    write(fd, &req, sizeof(Request));
-    close(fd);
-
-    // Espera resposta
-    int res_fd = open(client_pipe, O_RDONLY);
-    if (res_fd < 0) {
-        perror("open server_to_client");
-        return 1;
+    // Ler resposta do servidor
+    int client_fd = open(client_pipe, O_RDONLY);
+    if (client_fd < 0) {
+        perror("Erro ao abrir pipe do cliente");
+        exit(1);
     }
+    read(client_fd, res, sizeof(Response));
+    close(client_fd);
 
-    Response res;
-    read(res_fd, &res, sizeof(Response));
-    close(res_fd);
-
-    printf("Resposta do servidor: %s\n", res.response);
-
+    // Remover pipe do cliente
     unlink(client_pipe);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s [comando] [argumentos]\n", argv[0]);
+        return 1;
+    }
+
+    Request req = { .client_pid = getpid(), .op = OP_PING };
+    Response res = { .response = "" };
+
+    if (strcmp(argv[1], CMD_ADD) == 0 && argc == 6) {
+        // Indexação
+        req.op = OP_ADD;
+        snprintf(req.data, MAX_MESSAGE, "%s|%s|%s|%s", argv[2], argv[3], argv[4], argv[5]);
+    } else if (strcmp(argv[1], CMD_QUERY) == 0 && argc == 3) {
+        // Consulta
+        req.op = OP_QUERY;
+        snprintf(req.data, MAX_MESSAGE, "%s", argv[2]);
+    } else if (strcmp(argv[1], CMD_DELETE) == 0 && argc == 3) {
+        // Remoção
+        req.op = OP_DELETE;
+        snprintf(req.data, MAX_MESSAGE, "%s", argv[2]);
+    } else if (strcmp(argv[1], CMD_COUNT) == 0 && argc == 4) {
+        // Contagem de linhas
+        req.op = OP_COUNT;
+        snprintf(req.data, MAX_MESSAGE, "%s|%s", argv[2], argv[3]);
+    } else if (strcmp(argv[1], CMD_SEARCH) == 0 && argc == 3) {
+        // Pesquisa
+        req.op = OP_SEARCH;
+        snprintf(req.data, MAX_MESSAGE, "%s", argv[2]);
+    } else if (strcmp(argv[1], CMD_SHUTDOWN) == 0) {
+        // Encerramento
+        req.op = OP_SHUTDOWN;
+    } else {
+        printf("Comando inválido.\n");
+        return 1;
+    }
+
+    // Enviar pedido e exibir resposta
+    send_request(&req, &res);
+    printf("%s\n", res.response);
+
     return 0;
 }
